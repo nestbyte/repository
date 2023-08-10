f1ae102c9e13ff0317803d51f22ca8b5399889b4
Aryansh Mahato <contact@aryansh.dev>
2023-08-10 23:22:10 +0530

    feat: initial commit GH-1

---
+++ release.config.js
@@ -0,0 +1,25 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+};
+
+module.exports = config;
